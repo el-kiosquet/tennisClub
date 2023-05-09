84dd5841b1233cdd8e1435bb46dff3bdd0032e67
pi_pablo <picosilvestrepablo@gmail.com>
2023-05-09 23:37:31 +0200

    Created java packets:  - application: with Main.java (launcher)  - controller: with main.controller (controller of the first FXML shown)  - images and models: for images and for java classes  - view: for FXML documents

---
+++ src/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author picos
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/view/Main.fxml"));
+        Parent root = loader.load();
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("Tennis Club");
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
