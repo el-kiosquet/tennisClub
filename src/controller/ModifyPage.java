2af8be84fa30f5cd6e28579dcd56a592c5cb74f1
pi_pablo <picosilvestrepablo@gmail.com>
2023-05-15 23:39:58 +0200

    modify hecho

--- src/controller/ModifyPage.java
+++ src/controller/ModifyPage.java
@@ -6,9 +6,13 @@ package controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashSet;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -60,6 +64,12 @@ public class ModifyPage implements Initializable {
     private Button modifyButton;
 
     private Club club;
+    @FXML
+    private Label errorName;
+    @FXML
+    private Label errorSurname;
+    @FXML
+    private Label errorPhone;
     /**
      * Initializes the controller class.
      */
@@ -87,15 +97,104 @@ public class ModifyPage implements Initializable {
             Logger.getLogger(ModifyPage.class.getName()).log(Level.SEVERE, null, ex);
         }
         
+        modifyButton.setOnAction(this::modifyChanges);
+        
     }
 
     @FXML
     private void modifyChanges(ActionEvent event) {
+        //restore errorLabel values
+        errorName.setText("");
+        errorSurname.setText("");
+        errorPhone.setText("");
+        errorNickname.setText("");
+        errorPassword.setText("");
+        errorRepeatPassword.setText("");
+        errorSCV.setText("");
+
         
-        
+        if(validUser()){
+            member.setName(textName.getText());
+            member.setSurname(textSurname.getText());
+            member.setTelephone(textPhone.getText());
+            member.setPassword(textPassword.getText());
+            member.setSvc(Integer.parseInt(textSCV.getText()));
+            member.setCreditCard(textCreditCard.getText());   
+        }
     }
 
     @FXML
     private void cancel(ActionEvent event) {
+    }
+    
+    private boolean validUser(){
+        boolean isOk = true;
+        
+        // no checkins for name
+        // check only if the field is not empty
+        if(textName.getText().length() == 0){
+            isOk = false;
+            errorName.setText("put name");
+        }
+        
+        // no checkins for surname
+        // check only if the field is not empty
+        if(textSurname.getText().length() == 0){
+            isOk = false;
+            errorSurname.setText("put surname");
+        }
+        
+        // no checkins for telephone number
+        // check only if the field is not empty
+        if(textPhone.getText().length() == 0){
+            isOk = false;
+            errorPhone.setText("put telephone");
+        }  
+        
+        
+        
+        // password checkins:
+        // if it has letters and numbers
+        // length > 6
+        Matcher matcher = Pattern.compile("[0-9]+[a-z]|[a-z]+[0-9]", Pattern.CASE_INSENSITIVE).matcher(textPassword.getText());
+        if(textPassword.getText().length() <= 6){
+            isOk = false;
+            errorPassword.setText("password to short");
+        }
+        else if(!matcher.find()){
+            isOk = false;
+            errorPassword.setText("password must have letters and numbers");
+        }
+        
+        
+        //check repeated password
+        if(!textRepeatedPassword.getText().equals(textPassword.getText())){
+            errorRepeatPassword.setText("passwords don't coincide");
+        }
+        
+        // 16 numbers
+        // only numbers
+        matcher = Pattern.compile("[^0-9]").matcher(textCreditCard.getText());
+        if(textCreditCard.getText().length() != 16){
+            isOk = false;
+            errorCreditCard.setText("credit card must contain 16 numbers");
+        }
+        else if(matcher.find()){
+            isOk = false;
+            errorCreditCard.setText("credit card must contain only numbers");
+        }
+        
+        //check SCV
+        try{
+            int scv = Integer.parseInt(textSCV.getText());
+        }catch(NumberFormatException e){
+            if(textSCV.getText().length() != 0){
+                errorSCV.setText("scv must contain only 3 numbers");
+            } 
+        }
+        if(textSCV.getText().length() != 3 && textSCV.getText().length() != 0){errorSCV.setText("scv must contain only 3 numbers");}
+        
+        
+        return isOk;
     }
 }
---
+++ src/models/CheckMember.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author picos
+ */
+public class CheckMember {
+    
+    
+    public static boolean checkMember(){
+        boolean isOk = true;
+        
+        return isOk;
+    }
+}
